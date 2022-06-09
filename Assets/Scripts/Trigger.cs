0301cc3aed329eafa555a21711c0b78c0bc9ec86
M-Farhan-R <farhan.10119023@mahasiswa.unikom.ac.id>
2022-06-10 02:49:20 +0700

    Adding UI & Game Manager

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -2,14 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class reset : MonoBehaviour
+public class Trigger : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject ball, paddle1, paddle2;
+    public bool isPlayerRight;
+
+    public ScoreManager skorMenej;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("ball"))
+
+
+        if (collision.gameObject == ball)
         {
+            if (isPlayerRight)
+            {
+                skorMenej.AddScoreKiri();
+                
+            } else
+            {
+                skorMenej.AddScoreKanan();
+            }
             paddle1.GetComponent<PaddleControl>().Reset();
             paddle2.GetComponent<PaddleControl>().Reset();
             ball.GetComponent<BallControl>().Reset();
